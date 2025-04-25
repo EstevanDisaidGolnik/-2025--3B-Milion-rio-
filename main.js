cf8b547d6da054bfc9d8385b87c35fc00e70fcd1
Estevan Neri Disaid Emerenciano <141413662+EstevanDisaidGolnik@users.noreply.github.com>
2025-04-25 11:55:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const botoes = document.querySelectorAll(".botao")
 const textos = document.querySelectorAll(".aba-conteudo")
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-05T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2 ,tempoObjetivo3 ,tempoObjetivo4];
+ 
+for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i])
+}
+
+
 
 for(let i = 0; i< botoes.length; i++){
     botoes[i].onclick = function () {
@@ -11,4 +24,23 @@ for(let i = 0; i< botoes.length; i++){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo")
     }
+}
+
+function calculaTempo(tempoObjetivo){
+    
+
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    return dias  +  "Dias"  +  horas  +  "Horas"  +  minutos  +  "Minutos"  +  segundos  +  "Segundos";
+
 }
