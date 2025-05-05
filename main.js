9f058865f4a6d285699525267ccfe578a8d6bc34
Estevan Neri Disaid Emerenciano <141413662+EstevanDisaidGolnik@users.noreply.github.com>
2025-05-05 08:35:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ const botoes = document.querySelectorAll(".botao")
 const textos = document.querySelectorAll(".aba-conteudo")
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-1-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-05T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2 ,tempoObjetivo3 ,tempoObjetivo4];
- 
-for (let i=0; i<contadores.length;i++){
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+for (let i = 0; i < contadores.length; i++) {
     contadores[i].textContent = calculaTempo(tempos[i])
 }
 
 
 
-for(let i = 0; i< botoes.length; i++){
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for(let j = 0; j< botoes.length; j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
 
@@ -26,21 +26,41 @@ for(let i = 0; i< botoes.length; i++){
     }
 }
 
-function calculaTempo(tempoObjetivo){
-    
+function calculaTempo(tempoObjetivo) {
+
 
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
 
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-    segundos %=60;
-    minutos %=60;
-    horas %=24;
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
 
-    return dias  +  "Dias"  +  horas  +  "Horas"  +  minutos  +  "Minutos"  +  segundos  +  "Segundos";
+    if (tempoFinal > 0) {
 
+        return dias + "Dias" + horas + "Horas" + minutos + "Minutos" + segundos + "Segundos";
+    } else {
+        return "GG";
+    }
 }
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i])
+    }
+}
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
+//comecaCronometro();
+
